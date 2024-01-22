d57eefd8fcf60a389900a4e295e867f303915807
SplitInf <bann13@gmail.com>
2024-01-21 21:46:43 -0500

    update test

---
+++ test/test_Analysis.py
@@ -0,0 +1,8 @@
+import pytest
+from gitrepo3 import Analysis
+
+
+#test when non int arguement is provided to load_data
+def test_load_data_non_str_arguments():
+  with pytest.raises(TypeError):
+    Analysis.Analysis.(12341234)
